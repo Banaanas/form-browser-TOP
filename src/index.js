cb1ee855a9bcf90e7548a99da91ed2761261d24c
Cyril <cyrildeveloppeur@gmail.com>
2020-07-25 10:47:47 +0200

    Update Project

--- src/index.js
+++ src/index.js
@@ -26,78 +26,78 @@ function blockDisplayErrorDiv() {
 function showError(eventTargetID) {
   if (eventTargetID === "last-name") {
     if (lastName.validity.valueMissing) {
-      errorsDiv.textContent = "You need to enter your Last Name.";
+      errorsDiv.textContent = "You need to enter your Last Name";
     } else if (lastName.validity.patternMismatch) {
-      errorsDiv.textContent = "Only alphabetic characters are allowed.";
+      errorsDiv.textContent = "Only alphabetic characters are allowed";
     } else if (lastName.validity.tooShort) {
-      errorsDiv.textContent = `Last Name should be at least ${lastName.minLength} characters.`;
+      errorsDiv.textContent = `Last Name should be at least ${lastName.minLength} characters`;
     } else if (lastName.validity.valid) {
-      errorsDiv.textContent = "Last Name is alright.";
+      errorsDiv.textContent = "Last Name is alright";
     }
   }
 
   if (eventTargetID === "first-name") {
     if (firstName.validity.valueMissing) {
-      errorsDiv.textContent = "You need to enter your First Name.";
+      errorsDiv.textContent = "You need to enter your First Name";
     } else if (firstName.validity.patternMismatch) {
-      errorsDiv.textContent = "Only alphabetic characters are allowed.";
+      errorsDiv.textContent = "Only alphabetic characters are allowed";
     } else if (firstName.validity.tooShort) {
-      errorsDiv.textContent = `First Name should be at least ${firstName.minLength} characters.`;
+      errorsDiv.textContent = `First Name should be at least ${firstName.minLength} characters`;
     } else if (firstName.validity.valid) {
-      errorsDiv.textContent = "First Name is alright.";
+      errorsDiv.textContent = "First Name is alright";
     }
   }
 
   if (eventTargetID === "country") {
     if (country.validity.valueMissing) {
-      errorsDiv.textContent = "You need to enter your Country.";
+      errorsDiv.textContent = "You need to enter your Country";
     } else if (country.validity.patternMismatch) {
-      errorsDiv.textContent = "Only alphabetic characters are allowed.";
+      errorsDiv.textContent = "Only alphabetic characters are allowed";
     } else if (country.validity.tooShort) {
-      errorsDiv.textContent = `Country's Name should be at least ${country.minLength} characters.`;
+      errorsDiv.textContent = `Country's Name should be at least ${country.minLength} characters`;
     } else if (country.validity.valid) {
-      errorsDiv.textContent = "Country's Name is alright.";
+      errorsDiv.textContent = "Country's Name is alright";
     }
   }
 
   if (eventTargetID === "zip-code") {
     if (zipCode.validity.valueMissing) {
-      errorsDiv.textContent = "You need to enter your Zip Code.";
+      errorsDiv.textContent = "You need to enter your Zip Code";
     } else if (
       zipCode.validity.rangeUnderflow
       || zipCode.validity.rangeOverflow
     ) {
-      errorsDiv.textContent = "Zip-Code must be 5 numbers long.";
+      errorsDiv.textContent = "Zip-Code must be 5 numbers long";
     } else if (zipCode.validity.valid) {
-      errorsDiv.textContent = "Zip-Code is alright.";
+      errorsDiv.textContent = "Zip-Code is alright";
     }
   }
 
   if (eventTargetID === "e-mail") {
     if (eMail.validity.valueMissing) {
-      errorsDiv.textContent = "You need to enter your E-Mail address.";
+      errorsDiv.textContent = "You need to enter your E-Mail address";
     } else if (eMail.validity.typeMismatch) {
-      errorsDiv.textContent = "Entered value needs to be an E-Mail address.";
+      errorsDiv.textContent = "Entered value needs to be an E-Mail address";
     } else if (eMail.validity.tooShort) {
-      errorsDiv.textContent = `E-Mail should be at least ${eMail.minLength} characters.`;
+      errorsDiv.textContent = `E-Mail should be at least ${eMail.minLength} characters`;
     } else if (eMail.validity.valid) {
-      errorsDiv.textContent = "E-Mail is alright.";
+      errorsDiv.textContent = "E-Mail is alright";
     }
   }
 
   if (eventTargetID === "password") {
     if (password.validity.tooShort) {
-      errorsDiv.textContent = `Password should be at least ${password.minLength} characters. You entered ${password.value.length} yet.`;
+      errorsDiv.textContent = `Password should be at least ${password.minLength} characters. You entered ${password.value.length} yet`;
     } else if (password.validity.valid) {
-      errorsDiv.textContent = "Password is alright.";
+      errorsDiv.textContent = "Password is alright";
     }
   }
 
   if (eventTargetID === "confirm-password") {
     if (confirmPassword.validity.tooShort) {
-      errorsDiv.textContent = `Confirmed Password should be at least ${confirmPassword.minLength} characters; you entered ${confirmPassword.value.length}.`;
+      errorsDiv.textContent = `Confirmed Password should be at least ${confirmPassword.minLength} characters; you entered ${confirmPassword.value.length}`;
     } else if (confirmPassword.validity.valid) {
-      errorsDiv.textContent = "Confirmed Password is alright.";
+      errorsDiv.textContent = "Confirmed Password is alright";
     }
   }
 }
@@ -114,11 +114,11 @@ allLabels.forEach((item) => {
     blockDisplayErrorDiv();
     if (eventTargetID === "password" && !password.validity.tooShort) {
       if (confirmPassword.value !== password.value) {
-        errorsDiv.textContent = "Passwords don't match.";
+        errorsDiv.textContent = "Passwords don't match";
         return;
       }
       if (confirmPassword.value === password.value) {
-        errorsDiv.textContent = "Passwords match.";
+        errorsDiv.textContent = "Passwords match";
         return;
       }
     }
@@ -128,11 +128,11 @@ allLabels.forEach((item) => {
       && !confirmPassword.validity.tooShort
     ) {
       if (confirmPassword.value !== password.value) {
-        errorsDiv.textContent = "Passwords don't match.";
+        errorsDiv.textContent = "Passwords don't match";
         return;
       }
       if (confirmPassword.value === password.value) {
-        errorsDiv.textContent = "Passwords match.";
+        errorsDiv.textContent = "Passwords match";
         return;
       }
     }
@@ -150,7 +150,7 @@ allLabels.forEach((item) => {
       && confirmPassword.value === password.value
     ) {
       errorsDiv.innerHTML = ""; // Reset the content of the message
-      errorsDiv.textContent = "You can submit your form.";
+      errorsDiv.textContent = "You can submit your form";
     } else {
       // If there is still an error, show the correct error
       showError(eventTargetID);
@@ -192,7 +192,7 @@ allLabels.forEach((item) => {
       }
       // Set the Input indication Message
       errorsDiv.innerHTML = ""; // Reset the content of the message
-      errorsDiv.textContent = `Please, enter your ${eventTargetID}.`;
+      errorsDiv.textContent = `Please, enter your ${eventTargetID}`;
       blockDisplayErrorDiv();
     }
   });
@@ -214,7 +214,7 @@ allLabels.forEach((item) => {
       // In case there is an error message visible, if the field
       // is valid, remove the error message.
       errorsDiv.innerHTML = ""; // Reset the content of the message
-      errorsDiv.textContent = "You can submit your form.";
+      errorsDiv.textContent = "You can submit your form";
     }
   });
 });
@@ -234,6 +234,6 @@ submitForm.addEventListener("submit", (event) => {
     errorsDiv.innerHTML = "Passwords don't match";
   } else if (!submitForm.checkValidity()) {
     // If there is still an error, show the correct error
-    errorsDiv.innerHTML = "Please fill all the field with correct values.";
+    errorsDiv.innerHTML = "Please fill all the field with correct values";
   }
 });
